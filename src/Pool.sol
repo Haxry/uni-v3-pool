3d1916a6208f583dc18457a8930ce238d2a792e7
haxry <harrishbansal90600@gmail.com>
2025-01-03 18:04:07 +0000

    update func in tick.sol

--- src/Pool.sol
+++ src/Pool.sol
@@ -6,9 +6,13 @@ import "./lib/Tick.sol";
 import "./lib/Position.sol";
 import "./lib/SafeCast.sol";
 import "./interfaces/IERC20.sol";
+import "./lib/TickMath.sol";
 
 contract Pool{
     using SafeCast for int256;
+    using Tick for mapping(int24 => Tick.Info);
+    using Position for mapping(bytes32 => Position.Info);
+    using Position for Position.Info;
     address public immutable token0;
     address public immutable token1;
     uint24 public immutable fee;
@@ -25,6 +29,7 @@ contract Pool{
     }
 
     Slot0 public slot0;
+    mapping(int24 => Tick.Info) public ticks;
     mapping(bytes32 => Position.Info) public positions;
 
     modifier lock(){
@@ -43,6 +48,12 @@ contract Pool{
         maxLiquidityPerTick = Tick.tickSpacingToMaxLiquidityPerTick(_tickSpacing);
     }
 
+    function checkTicks(int24 tickLower, int24 tickUpper) private pure {
+        require(tickLower < tickUpper, 'TLU');
+        require(tickLower >= TickMath.MIN_TICK, 'TLM');
+        require(tickUpper <= TickMath.MAX_TICK, 'TUM');
+    }
+
     function initialize(uint160 sqrtPriceX96) external {
         //check that it is called only once 
         require(slot0.sqrtPriceX96 == 0, "Pool: already initialized");
@@ -57,8 +68,63 @@ contract Pool{
         int128 liquidityDelta;
     }
 
+    function _updatePosition(
+        address owner,
+        int24 tickLower,
+        int24 tickUpper,
+        int128 liquidityDelta,
+        int24 tick
+    ) private returns (Position.Info storage position) {
+        position = positions.get(owner, tickLower, tickUpper);
+
+        uint256 _feeGrowthInside0X128 = 0;
+        uint256 _feeGrowthInside1X128 = 0;
+
+        bool flippedLower;
+        bool flippedUpper;
+        if(liquidityDelta != 0){
+            flippedLower = ticks.update(
+                tickLower,
+                tick,
+                liquidityDelta,
+                _feeGrowthInside0X128,
+                _feeGrowthInside1X128,
+                false,
+                maxLiquidityPerTick
+            );
+            flippedUpper = ticks.update(
+                tickUpper,
+                tick,
+                liquidityDelta,
+                _feeGrowthInside0X128,
+                _feeGrowthInside1X128,
+                true,
+                maxLiquidityPerTick
+            );
+        }
+
+        position.update(liquidityDelta, _feeGrowthInside0X128, _feeGrowthInside1X128);
+        if (flippedLower) {
+            ticks.clear(tickLower);
+        }
+        if (flippedUpper) {
+            ticks.clear(tickUpper);
+        }
+    
+    }
+
     function _modifyPosition(ModifyPositionParams memory params) private returns (Position.Info storage position,int256 amount0,int256 amount1) 
     {
+        checkTicks(params.tickLower, params.tickUpper);
+        Slot0 memory _slot0 = slot0;
+        position = _updatePosition(
+            params.owner,
+            params.tickLower,
+            params.tickUpper,
+            params.liquidityDelta,
+            _slot0.tick
+        );
+
         return (positions[bytes32(0)], 0, 0);
     }
 
